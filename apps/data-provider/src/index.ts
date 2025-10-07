be51d605c0dd34f1f986b747058cf758217b2ce6
g122622 <guoyi122622@gmail.com>
2025-10-07 20:44:42 +0800

    feat: QQ聊天记录读取

--- apps/data-provider/src/index.ts
+++ apps/data-provider/src/index.ts
@@ -1,1 +1,9 @@
 import Logger from "@root/common/util/Logger";
+import { QQProvider } from "./providers/QQProvider";
+
+(async () => {
+    Logger.info("QQProvider init");
+    const qqProvider = new QQProvider();
+    await qqProvider.init();
+    Logger.info("QQProvider init done");
+})();
--- apps/data-provider/src/providers/QQProvider.ts
+++ apps/data-provider/src/providers/QQProvider.ts
@@ -4,18 +4,19 @@ import { IIMProvider } from "./@types/IIMProvider";
 const Database = require("@journeyapps/sqlcipher").verbose().Database;
 
 export class QQProvider implements IIMProvider {
-    private db = null;
+    private db: typeof Database;
 
     public async init() {
         const dbPath = (await ConfigManagerService.getCurrentConfig()).dataProviders.QQ.dbBasePath + "/nt_msg.db";
         // 打开数据库（原地读取，不复制）
+        // @see https://docs.aaqwq.top/decrypt/decode_db.html#%E9%80%9A%E7%94%A8%E9%85%8D%E7%BD%AE%E9%80%89%E9%A1%B9
         const db = new Database(dbPath, {
-            // 必须设置 SQLCipher 选项
-            cipher: "sqlcipher",
+            cipher: "aes-256-cbc",
             key: (await ConfigManagerService.getCurrentConfig()).dataProviders.QQ.dbKey,
-            // 可选：如果你知道 SQLCipher 版本（NTQQ 一般用 v4）
             cipher_page_size: 4096,
-            kdf_iter: 64000 // SQLCipher v4 默认值
+            kdf_iter: 4000, // 非默认值
+            cipher_hmac_algorithm: "HMAC_SHA1", // 非默认值
+            cipher_default_kdf_algorithm: "PBKDF2_HMAC_SHA512" // 非默认值
         });
         this.db = db;
     }
