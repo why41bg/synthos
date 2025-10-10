31ecbfb0125274219f4370fbc56c3ab8132a9b82
g122622 <guoyi122622@gmail.com>
2025-10-10 17:53:08 +0800

    feat: QQ聊天数据库成功解密！

--- common/config/@types/GlobalConfig.ts
+++ common/config/@types/GlobalConfig.ts
@@ -3,7 +3,6 @@ export interface GlobalConfig {
         QQ: {
             dbBasePath: string; // NTQQ存放数据库的文件夹路径（不是某个数据库文件的路径）
             dbKey: string; // NTQQ的数据库密钥
-            ramdiskPath: string; // 用于暂存ramdisk路径
         };
     };
 }
