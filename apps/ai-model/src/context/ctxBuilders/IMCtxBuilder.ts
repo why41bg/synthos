936bd27b3d7a45b18ac18bba23070e949daf278f
g122622 <guoyi122622@gmail.com>
2025-10-13 17:18:25 +0800

    feat: ai-model项目结构搭建

---
+++ apps/ai-model/src/context/ctxBuilders/IMCtxBuilder.ts
@@ -0,0 +1,15 @@
+import { ProcessedChatMessageWithRawMessage } from "@root/common/types/data-provider";
+import { ICtxBuilder } from "./@types/ICtxBuilder";
+import { IMPromptStore } from "../prompts/IMPromptStore";
+
+export class IMCtxBuilder implements ICtxBuilder {
+    async init(): Promise<void> {}
+    async buildCtx(messages: ProcessedChatMessageWithRawMessage[]): Promise<string> {
+        let content = "";
+        for (const message of messages) {
+            content += message.preProcessedContent + "\n";
+        }
+        return IMPromptStore.getSummarizePrompt(5, content);
+    }
+    async close(): Promise<void> {}
+}
--- apps/ai-model/src/context/prompts/IMPromptStore.ts
+++ apps/ai-model/src/context/prompts/IMPromptStore.ts
@@ -1,4 +1,4 @@
-export class PromptStore {
+export class IMPromptStore {
     // TODO 补充背景信息，如：wj代表陈彦希、这个群的简介、主要话题方向、主要人员构成等
     public static getSummarizePrompt(maxTopics: number, messages: string): string {
         return `你是一个帮我进行群聊信息总结的助手，生成总结内容时，你需要严格遵守下面的几个准则：
